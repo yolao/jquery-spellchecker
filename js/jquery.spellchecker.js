2c8c6ad36962f52ed30164d7e82589baae35c7c1
willis.rh <willis.rh@a7ef4894-cf05-11dd-8772-d1b041bf0e37>
2009-12-01 12:19:19 +0000

    cleaning up, moving things around, adding comments

--- js/jquery.spellchecker.js
+++ js/jquery.spellchecker.js
@@ -1,7 +1,7 @@
 /*
  * 
  * jquery.spellchecker.js - a simple jQuery Spell Checker
- * Copyright (c) 2008 Richard Willis
+ * Copyright (c) 2009 Richard Willis
  * MIT license  : http://www.opensource.org/licenses/mit-license.php
  * Project      : http://jquery-spellchecker.googlecode.com
  * Contact      : willis.rh@gmail.com
@@ -15,7 +15,6 @@
 		spellcheck : function(options, callback){
 
 			return this.each(function(){
-
 				if ($(this).data('spellchecker') && $(this).data("spellchecker")[options]){
 					$(this).data("spellchecker")[options](callback);
 				} else {
@@ -26,6 +25,8 @@
 
 		}
 	});
+
+	// note, perhaps only 1 instance of the spellchecker would be better??
 
 	var SpellChecker = function(domObj, options) {
 		this.domObj = domObj;
@@ -40,50 +41,25 @@
 
 	SpellChecker.prototype = {
 
+		// initial setup
 		init : function(){
-			this.createElements();
-		},
-
-		createElements : function(){
 			var self = this;
-			this.elements.$suggestWords = 
-				$("<div></div>").attr({id: "suggestwords"});
-			this.elements.$ignoreWord = 
-				$("<a></a>").attr({ title: "ignore word", href: "#" })
-				.mousedown(function(){self.ignore();}).text("Ignore word");
-			this.elements.$ignoreAllWords = 
-				$("<a></a>").attr({ title: "ignore all words", href: "#"})
-				.mousedown(function(){self.ignoreAll();}).text("Ignore all");
-			this.elements.$ignoreWordsForever = 
-				$("<a></a>").attr({title: "ignore word forever (add to dictionary)", href: "#"})
-				.mousedown(function(){self.addToDictionary;}).text("Ignore forever");
-			this.elements.$suggestFoot = 
-				$("<div></div>").attr({id: "suggestfoot", class: "foot"})
-				.append(this.elements.$ignoreWord)
-				.append(this.elements.$ignoreAllWords)
-				.append(this.options.engine == "pspel" ? this.elements.$ignoreWordsForever : false);
-			this.elements.$suggestBox = 
-				$("<div></div>").attr({id: "suggestbox", class: "suggestDrop"})
-				.append(this.elements.$suggestWords)
-				.append(this.elements.$suggestFoot)
-				.prependTo("body");
+			this.createElements();
+			// hide the suggest box on document click
+			$(document).bind("click", function(e){
+				if (!$(e.target).hasClass(".spellcheck-badspelling") && !$(e.target).parents().filter(".suggestDrop").length) {
+					self.hideBox();
+				}
+			});
 		},
 
+		// checks a chunk of text for bad words, then either shows the words below the original element (if texarea) or highlights the bad words
 		check : function(callback){
 
-			var self = this, node = this.domObj.nodeName, text, html;
-
-			text = $.trim(((node != "TEXTAREA" && node != "INPUT") ? 
-				$(this.domObj).text() : 
-				$(this.domObj).val()).replace(/^\W|[\W]+\W|\W$|\n|\t|\s{2,}/g, " "));
-
-			html = (node != "TEXTAREA" && node != "INPUT") ? 
-				$(this.domObj).html() : 
-				$(this.domObj).val().replace(/\n/g, "<br />");
-
+			var self = this, node = this.domObj.nodeName;
+		
 			if (node == "TEXTAREA") {
-				this.type = 'textarea';
-				this.checkText(text, function(json){
+				this.checkTextarea(function(text, json) {
 					if (!self.elements.$badwords) {
 						self.elements.$badwords = $("<div></div>").attr("id", "spellcheck-badwords");
 						$(self.domObj).after(self.elements.$badwords);
@@ -102,8 +78,8 @@
 					(callback) && callback();
 				});
 			} else {
-				this.type = 'html';
-				this.checkText(text, function(json){
+				var html = $(this.domObj).html();
+				this.checkHTML(function(text, json){
 					var replace = "";
 					// highlight bad words
 					for(var badword in json) {
@@ -124,13 +100,27 @@
 					(callback) && callback();
 				});
 			}
-			$(document).bind("click", function(e){
-				if (!$(e.target).hasClass(".spellcheck-badspelling") && !$(e.target).parents().filter(".suggestDrop").length) {
-					self.hideBox();
-				}
+		},
+
+		// grabs, munges and checks textarea text, returns JSON object of bad words
+		checkTextarea : function(callback){
+			this.type = 'textarea';
+			var text = $.trim($(this.domObj).val().replace(/^\W|[\W]+\W|\W$|\n|\t|\s{2,}/g, " ")); // strip punctuation
+			this.checkText(text, function(json){
+				(callback) && callback(text, json);
 			});
 		},
 
+		// grabs, munges and checks HTML text, returns JSON object of bad words
+		checkHTML : function(callback){
+			this.type = 'html';
+			var text = $.trim($(this.domObj).text().replace(/^\W|[\W]+\W|\W$|\n|\t|\s{2,}/g, " ")); // strip punctuation
+			this.checkText(text, function(json) {
+				(callback) && callback(text, json);
+			});
+		},
+
+		// gets a list of suggested words, appends to the suggestbox and shows the suggestbox
 		suggest : function(domObj){
 
 			var self = this, $domObj = $(domObj), offset = $domObj.offset();
@@ -151,7 +141,10 @@
 				for(var i=0;i<(json.length<5?json.length:5);i++) {
 					var $replaceWord = $("<a></a>")
 						.attr({href: "#",class: (!i?'first':'')})
-						.mousedown(function(){self.replace(domObj, this);})
+						.click(function(e){
+							e.preventDefault();
+							self.replace(domObj, this);
+						})
 						.text(json[i]);
 					self.elements.$suggestWords.append($replaceWord);
 				}								
@@ -170,6 +163,7 @@
 			});
 		},
 
+		// sends post request to check a chunk of text, return JSON object of incorrectly spelt words
 		checkText : function(text, callback){
 			var self = this,
 			xhr = $.ajax({
@@ -192,6 +186,7 @@
 			return xhr;
 		},
 
+		// sends post request to get single word suggestions, return JSON object of suggested words
 		getWordSuggestions : function(text, callback) {
 			var self = this,
 			xhr = $.ajax({
@@ -223,26 +218,27 @@
 				case "textarea": this.replaceTextbox(domObj, replace); break;
 				case "html" : this.replaceHtml(domObj, replace); break;
 			}
-
 		},
 
+		// replace word in a textarea
 		replaceTextbox : function(domObj, replace){
 			$(domObj).next().remove();
 			$(domObj).remove();
 			$(this.domObj).val($(this.domObj).val().replace(new RegExp(domObj.innerHTML, "i"), replace.innerHTML));
 		},
 
+		// replace word in an HTML container that is not a form field
 		replaceHtml : function(domObj, replace){
 			$(domObj).after(replace.innerHTML).remove();
 		},
 		
-		// build & show word suggestion box 
+		// remove spelling formatting from word to ignore in original element
 		ignore : function() {
 			this.$curWord.after(this.$curWord.html()).remove();
 			this.hideBox();
 		},
 		
-		// ignore all words in this chunk of text
+		// remove seplling formatting from all words to ignore in original element
 		ignoreAll : function() {
 			var self = this;
 			$("span.spellcheck-badspelling", self.domObj).each(function(){
@@ -274,7 +270,52 @@
 			$("span.spellcheck-badspelling", this.domObj).each(function(){
 				$(this).after(this.innerHTML).remove()
 			});
+		},
+
+		// creates the suggestbox
+		createElements : function(){
+			var self = this;
+			this.elements.$suggestWords = 
+				$("<div></div>")
+				.attr({id: "suggestwords"});
+			this.elements.$ignoreWord = 
+				$("<a></a>")
+				.attr({ title: "ignore word", href: "#" })
+				.click(function(e){
+					e.preventDefault();
+					self.ignore();
+				})
+				.text("Ignore word");
+			this.elements.$ignoreAllWords = 
+				$("<a></a>")
+				.attr({ title: "ignore all words", href: "#"})
+				.click(function(e){
+					e.preventDefault();
+					self.ignoreAll();
+				})
+				.text("Ignore all");
+			this.elements.$ignoreWordsForever = 
+				$("<a></a>")
+				.attr({title: "ignore word forever (add to dictionary)", href: "#"})
+				.click(function(e){
+					e.preventDefault();
+					self.addToDictionary();
+				})
+				.text("Ignore forever");
+			this.elements.$suggestFoot = 
+				$("<div></div>")
+				.attr({id: "suggestfoot", class: "foot"})
+				.append(this.elements.$ignoreWord)
+				.append(this.elements.$ignoreAllWords)
+				.append(this.options.engine == "pspel" ? this.elements.$ignoreWordsForever : false);
+			this.elements.$suggestBox = 
+				$("<div></div>")
+				.attr({id: "suggestbox", class: "suggestDrop"})
+				.append(this.elements.$suggestWords)
+				.append(this.elements.$suggestFoot)
+				.prependTo("body");
 		}
+
 	};	
 
 })(jQuery);
